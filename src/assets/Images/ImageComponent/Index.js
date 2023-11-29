bcee5198dfce0a30dcf95ce8030d94cb29d21d74
Shahd Dawod <71906350+shahd123-png@users.noreply.github.com>
2023-11-29 11:26:31 +0200

    Add files via upload

---
+++ src/assets/Images/ImageComponent/Index.js
@@ -0,0 +1,13 @@
+import Lamb from "../ImageJPG/LoginAssets.png"
+import Person from '../ImageJPG/PersonLogin.png'
+import Avatar from '../ImageJPG/LoginAvatar.png'
+import Rectangle  from "../ImageJPG/Rectangle.png";
+
+const ImagesComponent = {
+Lamb,
+Person,
+Avatar,
+Rectangle,
+};
+
+export default ImagesComponent
